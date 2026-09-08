3cfd10cd77ba97346a95f8dc61f5fd6efa45d2d5
a0955523155-pixel <a0955523155@gmail.com>
2026-09-08 14:18:38 +0800

    更新

--- js/line-config.js
+++ js/line-config.js
@@ -2,7 +2,7 @@
 // 建立 LIFF App 後，把 LIFF ID 貼在這裡即可啟用 LINE 內建功能。
 // LIFF ID 不是 secret，可安全放在前端。
 export const lineConfig = {
-  liffId: "YOUR_LIFF_ID",
+  liffId: "2011502071-EM878xNE",
   officialLineUrl: "https://line.me/ti/p/~287ppyfa"
 };
 
